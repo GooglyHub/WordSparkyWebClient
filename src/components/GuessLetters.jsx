ed647957ce2618edc4acd8088b4bcdfbfbe8c147
Ho, Randy <googlypower@gmail.com>
2021-07-12 00:32:52 -0700

    Combined GuessLetters and LettersInput so that GameBody can capture keyboard events

--- src/components/GuessLetters.jsx
+++ src/components/GuessLetters.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import CardHeader from './CardHeader';
 import GameBody from './GameBody';
 import utils from '../common/utils';
-import LettersInput from './LettersInput';
 import {
     guessLetters,
     guessLettersGuest,
@@ -10,16 +9,51 @@ import {
 } from '../services/gamesService';
 import { getCurrentUser } from '../services/authService';
 import { deleteGame } from '../services/gamesService';
+import AppKeyboard from './AppKeyboard';
+import colors from './../config/colors';
+import GameCell from './GameCell';
+
+const NUM_SLOTS = 6;
 
 class GuessLetters extends Component {
+    createInitialLetters() {
+        let initialLetters = [];
+        for (let i = 0; i < NUM_SLOTS; ++i) {
+            initialLetters.push('?');
+        }
+        return initialLetters;
+    }
+
     state = {
         expanded: this.props.expandedInitially,
+        letters: this.createInitialLetters(),
         error: null,
+    };
+
+    createCell = (ch) => {
+        if (ch === '?') {
+            return {
+                state: 'unknown',
+                key: '?',
+                isPressable: false,
+            };
+        } else {
+            return {
+                state: 'guess',
+                key: ch,
+                isPressable: false,
+            };
+        }
     };
 
     // This is called once the user has chosen the six letters
     // to begin a solve (e.g. R-S-T-L-N-E)
-    handleChosenLetters = async (letters) => {
+    handleChosenLetters = async () => {
+        let letters = '';
+        for (let i = 0; i < NUM_SLOTS; ++i) {
+            letters += this.state.letters[i];
+        }
+
         const user = getCurrentUser();
         if (user) {
             try {
@@ -51,6 +85,43 @@ class GuessLetters extends Component {
         }
     };
 
+    onKeyPress = (key) => {
+        // need to check that the key is not disabled
+        if (
+            key.length > 1 ||
+            (key.length === 1 && !this.state.letters.includes(key))
+        ) {
+            this.handlePress({ key });
+        }
+    };
+
+    handlePress = (data) => {
+        const key = data.key;
+        if (key === 'DEL') {
+            for (let i = NUM_SLOTS - 1; i >= 0; --i) {
+                if (this.state.letters[i] !== '?') {
+                    const newLetters = [...this.state.letters];
+                    newLetters[i] = '?';
+                    this.setState({ letters: newLetters });
+                    break;
+                }
+            }
+        } else if (key === 'Enter') {
+            if (NUM_SLOTS > 0 && this.state.letters[NUM_SLOTS - 1] !== '?') {
+                this.handleChosenLetters();
+            }
+        } else {
+            for (let i = 0; i < NUM_SLOTS; ++i) {
+                if (this.state.letters[i] === '?') {
+                    const newLetters = [...this.state.letters];
+                    newLetters[i] = key;
+                    this.setState({ letters: newLetters });
+                    break;
+                }
+            }
+        }
+    };
+
     setExpanded = (expanded) => {
         this.setState({ expanded });
         toggleExpansion(this.props.gameId, expanded);
@@ -63,7 +134,6 @@ class GuessLetters extends Component {
             <>
                 <CardHeader
                     onClick={() => {
-                        //this.setExpanded(!this.state.expanded);
                         this.setExpanded(true);
                     }}
                     title={`${creator} sent a puzzle (${utils.getAgeString(
@@ -80,12 +150,77 @@ class GuessLetters extends Component {
                     onClick={() => {
                         this.setExpanded(true);
                     }}
+                    onKeyPress={this.onKeyPress}
                 ></GameBody>
                 {this.state.expanded && (
-                    <LettersInput
-                        numSlots={6}
-                        onChosenLetters={this.handleChosenLetters}
-                    />
+                    <>
+                        <CardHeader title={`Choose ${NUM_SLOTS} letters`} />{' '}
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                flexWrap: 'wrap',
+                                paddingLeft: 10,
+                                paddingVertical: 5,
+                                backgroundColor: colors.secondary,
+                                alignItems: 'center',
+                            }}
+                            tabIndex="0"
+                            onKeyUp={(event) => {
+                                event.stopPropagation();
+                                let key = event.key;
+                                if (
+                                    key.length === 1 &&
+                                    key >= 'a' &&
+                                    key <= 'z'
+                                ) {
+                                    key = key.toUpperCase();
+                                }
+                                if (key === 'Backspace') {
+                                    this.onKeyPress('DEL');
+                                } else if (key === 'Enter') {
+                                    this.onKeyPress('Enter');
+                                } else if (
+                                    key.length === 1 &&
+                                    key >= 'A' &&
+                                    key <= 'Z'
+                                ) {
+                                    this.onKeyPress(key);
+                                }
+                            }}
+                        >
+                            {this.state.letters.map((letter, idx) => (
+                                <GameCell
+                                    cell={this.createCell(letter)}
+                                    marginHorizontal={3}
+                                    key={idx}
+                                />
+                            ))}
+                            <div
+                                style={{
+                                    marginLeft: 10,
+                                    borderWidth: 2,
+                                    borderColor: colors.primary,
+                                }}
+                            >
+                                <button
+                                    className="btn btn-primary"
+                                    onClick={this.handleSubmit}
+                                    disabled={
+                                        NUM_SLOTS > 0 &&
+                                        this.state.letters[NUM_SLOTS - 1] ===
+                                            '?'
+                                    }
+                                >
+                                    Submit
+                                </button>
+                            </div>
+                        </div>{' '}
+                        <AppKeyboard
+                            disabledKeys={this.state.letters}
+                            onPress={this.handlePress}
+                        />
+                    </>
                 )}
                 {this.state.error && (
                     <div
